1aef2740297511a4366d1c7e7015fee91aa2717d
xianZhao525 <wanwen_ji@163.com>
2026-01-14 15:10:27 +0800

    init：项目初始创建

---
+++ knowledge-base/src/test/java/com/xianzhao/knowledge_base/KnowledgeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xianzhao.knowledge_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
