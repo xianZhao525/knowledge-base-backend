de972c7a9d199aa24b7533d5a1c57a8d9d9a70d7
xianZhao525 <wanwen_ji@163.com>
2026-01-15 15:57:17 +0800

    fix：完善Knowledge.java文件

--- knowledge-base/src/main/java/com/xianzhao/knowledge_base/entity/Knowledge.java
+++ knowledge-base/src/main/java/com/xianzhao/knowledge_base/entity/Knowledge.java
@@ -1,8 +1,47 @@
 package com.xianzhao.knowledge_base.entity;
 
 import java.lang.String;
-import java.time.LocalDataTime;
+import java.time.LocalDateTime;  
 
 public class Knowledge {
-    private LocalDtateTime createdAt;
+    private Long id;
+    private String title;//标题
+    private String content;//内容
+    private LocalDateTime createdAt;//创建时间
+
+    public Knowledge(){
+
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setIdf(Long id){
+        this.id=id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public void setContent(String content){
+        this.content=content;
+    }
+
+    public LocalDateTime getCreateAt(){
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt=createdAt;
+    }
 }
