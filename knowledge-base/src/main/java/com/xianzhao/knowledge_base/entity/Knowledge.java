e860f3110ca94fe006d52f1fc541132ae7a74d7b
xianZhao525 <wanwen_ji@163.com>
2026-01-16 16:32:10 +0800

    fix：完善数据库调用

--- knowledge-base/src/main/java/com/xianzhao/knowledge_base/entity/Knowledge.java
+++ knowledge-base/src/main/java/com/xianzhao/knowledge_base/entity/Knowledge.java
@@ -1,12 +1,32 @@
 package com.xianzhao.knowledge_base.entity;
 
 import java.lang.String;
-import java.time.LocalDateTime;  
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;  
+//告诉JPA这是一个实体类，对应数据库中的一张表
+@Entity
+//指定这张表的名字是knowledge
+@Table(name="knowledge")
 public class Knowledge {
+    @Id
+    //主键生成策略：数据库自增
+    //IDENTITY = MySQL 的 AUTO_INCREMENT
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false,length = 255)
     private String title;//标题
+
+    @Column(columnDefinition = "TEXT")
     private String content;//内容
+
+    @Column(name = "created_at")
     private LocalDateTime createdAt;//创建时间
 
     public Knowledge(){
